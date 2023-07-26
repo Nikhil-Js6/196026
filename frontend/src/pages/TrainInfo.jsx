b935b91444dae3daa82ed5d5e5841c06f5e60b30
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2023-07-26 19:27:31 +0530

    Add files via upload

---
+++ frontend/src/pages/TrainInfo.jsx
@@ -0,0 +1,13 @@
+import SingleTrain from "../components/SingleTrain.jsx/SingleTrain";
+import './trainInfo.css';
+
+const TrainInfo = () => {
+
+    return (
+        <div className='trainInfo'>
+            <SingleTrain/>
+        </div>
+    )
+}
+
+export default TrainInfo
