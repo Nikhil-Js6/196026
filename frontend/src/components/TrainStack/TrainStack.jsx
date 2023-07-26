2d744a83ecc37e35846ae6c54c3a544902cd69b2
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2023-07-26 19:28:31 +0530

    Create TrainStack.jsx

---
+++ frontend/src/components/TrainStack/TrainStack.jsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+import Train from '../TrainCard/Train';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  width: '65%',
+  height: '50%',
+  alignSelf: 'center',
+}));
+
+export default function TrainStack({ trains }) {
+  return (
+    <Box sx={{ width: '100%', alignSelf: 'center' }}>
+      <Stack spacing={2}>
+        {
+          trains.map(train => (
+            <Item>
+               <Train train={train}/>
+            </Item>
+          ))
+        }
+      </Stack>
+    </Box>
+  );
+}
